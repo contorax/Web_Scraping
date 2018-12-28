91d84b940c6b95d6634db008cbe15fc1085acc86
contorax <contorax@yandex.com>
2018-12-28 00:26:04 -0700

    scraped quotes from the web

--- quotes.py
+++ quotes.py
@@ -2,5 +2,20 @@ from bs4 import BeautifulSoup
 import requests
 
 res = requests.get  ('http://quotes.toscrape.com/')
+soup = BeautifulSoup (res.text, 'lxml')
 
-print(res)
+quote = soup.find_all ('div', {'class': 'quote'})
+
+for q in quote:
+	message = q.find ('span', {'class': 'text'})
+	# mes = message.decode("windows-1252")
+	print (message.text)
+
+	avtor = q.find ('small')
+	print ("*** " + avtor.text + " ***")
+	# print (avtor.text.encode('utf-8'))
+	print ()
+	
+
+
+
